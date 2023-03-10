b490cc0e0a213a559a3b895795a19e43fc14b073
Rick Castaneda <rickcastaneda@outlook.com>
2023-03-09 22:16:59 -0600

    Add new scenarios using Predicate to filter elements from stream

--- src/com/castanedadev/lambdas/StreamsAndFilters.java
+++ src/com/castanedadev/lambdas/StreamsAndFilters.java
@@ -2,11 +2,31 @@ package com.castanedadev.lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 
 public class StreamsAndFilters {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		List<String> strawHatPirates = Arrays.asList("Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Robin", "", null);
-		strawHatPirates.stream().filter(member -> member != null).filter(member -> member != "")
-				.forEach(System.out::println);
+		AtomicInteger aInt = new AtomicInteger(1);
+		List<String> strawHatPirates = Arrays.asList("Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Robin", "Jinbei", "", null);
+
+		HttpResponse<JsonNode> response = Unirest.get("https://animechan.vercel.app/api/quotes/anime?title=One Piece")
+				.asJson();
+		
+		// Print all quotes from One Piece
+		response.getBody().getArray().forEach(quote -> System.out.printf("\n#%d %s", aInt.getAndIncrement(), quote));
+
+		// Print only character field from each one piece quote object
+		response.getBody().getArray().forEach(quote -> System.out.println(((JSONObject) quote).get("character")));
+
+		// Print all quotes from straw hat pirates members only
+		Predicate<JSONObject> isStrawHatPirate = quote -> strawHatPirates.contains(quote.get("character"));
+		response.getBody().getArray().toList().stream().filter(isStrawHatPirate).forEach(System.out::println);
 	}
 }
